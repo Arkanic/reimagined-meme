d1421833619ddc7c89563569b9458b91cd51ef05
Arkanic <silverfishasaurus@gmail.com>
2021-03-18 03:34:12 +0000

    things

--- src/client/index.ts
+++ src/client/index.ts
@@ -6,29 +6,27 @@ import * as input from "./ts/input";
 import Logger from "./ts/logger";
 
 console.log("%cREIMAGINED_MEME", "color:blue; font-size:32px; font-weight:bold;", "\nBy Arkanic"); // hehe
-console.log("\n\n%cStop!%c\nYOUR ACCOUNT DATA COULD BE AT RISK.\n%cIf someone told you to copy and paste something here, it is most likely malware. If you continue, there is a highly likely chance that your game data will be stolen.%c\nProceed at your own risk.\n\n", "color:red; font-size: 28px; font-weight:bold;", "color:black; font-size:16px; font-family:sans-serif;", "color:black; font-size:12px;", "color:red; font-size:12px; font-weight:bold");
+console.log("\n\n%cStop!%c\nYOUR ACCOUNT DATA COULD BE AT RISK.\n%cIf someone told you to copy and paste something here, it is most likely malware. If you continue, there is a highly likely chance that your game data will be stolen.%c\nProceed at your own risk.\n\n",
+    "color:red; font-size: 28px; font-weight:bold;", "color:black; font-size:16px; font-family:sans-serif;", "color:black; font-size:12px;", "color:red; font-size:12px; font-weight:bold");
 
 let logger:Logger = new Logger("main", "red");
-logger.log("testing 123");
-logger.error("fake error");
+
+const playMenu:HTMLElement = document.getElementById("play-menu")!;
+const playButton:HTMLButtonElement = <HTMLButtonElement>document.getElementById("play-button")!;
+const usernameInput:HTMLInputElement = <HTMLInputElement>document.getElementById("username-input")!;
 
 Promise.all([
     networking.connect(),
     render.setup()
 ]).then(() => {
-    input.startInputHandling();
-    render.startRendering();
-    networking.play({username:"", screenWidth:window.innerWidth, screenHeight:window.innerHeight});
-    networking.updateInput({
-        mouseX:0,
-        mouseY:0,
-        clicking:false,
-
-        keys: {
-            w:true,
-            a:true,
-            s:false,
-            d:false
-        }
+    playMenu.classList.remove("hidden");
+    usernameInput.focus();
+    
+    playButton.addEventListener("click", () => {
+        networking.play({username:usernameInput.value, screenWidth:window.innerWidth, screenHeight:window.innerHeight});
+        logger.log(`Game started. "${usernameInput.value}" @${window.innerWidth}x${window.innerHeight}`);
+        playMenu.classList.add("hidden");
+        input.startInputHandling();
+        render.startRendering();
     });
-});
+}).catch(logger.error);
